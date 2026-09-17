df4786a6c29ef6226c08f2fd4d7e12fb5f02d7a5
kamelia <kameliaali31@gmail.com>
2026-09-17 22:16:48 +0300

    update

--- emotion-ui/src/App.jsx
+++ emotion-ui/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
     try {
       // Send the image to our Flask API
-      const response = await fetch('http://127.0.0.1:5000/analyze', {
+      const response = await fetch('https://fast-chefs-act.loca.lt', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
